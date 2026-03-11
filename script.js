b8b24a9258c16b235cef4358adeb43bc7cf19335
Bosu <badebosubabu@gmail.com>
2026-03-11 13:31:45 -0400

    add the feature request loan by using the some method

--- script.js
+++ script.js
@@ -161,6 +161,16 @@ btnTransfer.addEventListener('click', function (e) {
   }
 });
 
+//loan using some method
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+  const amount = Number(inputLoanAmount.value);
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    currentAccount.movements.push(amount);
+    updateUi(currentAccount);
+  }
+  inputLoanAmount.value = '';
+});
 btnClose.addEventListener('click', function (e) {
   e.preventDefault();
   if (
